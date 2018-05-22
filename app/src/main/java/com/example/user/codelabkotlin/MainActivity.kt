a892024c02e789fd4455b4c637861fd54a306316
JP Hashagen <jayp33e@gmail.com>
2018-05-22 20:57:25 +0200

    Add button

--- app/src/main/java/com/example/user/codelabkotlin/MainActivity.kt
+++ app/src/main/java/com/example/user/codelabkotlin/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.user.codelabkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeText(view : android.view.View) {
+        val tv = findViewById<TextView>(R.id.textView)
+
+        if (tv.text.equals("Hello World!"))
+            tv.text = "Hello SoundCloud!"
+        else
+            tv.text = "Hello World!"
     }
 }
