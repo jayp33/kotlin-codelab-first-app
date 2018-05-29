dc2116f9b153a4ad9582ff5e4457aca97d63f75c
JP Hashagen <jayp33e@gmail.com>
2018-05-29 20:30:19 +0200

    Count up

--- app/src/main/java/com/example/user/codelabkotlin/MainActivity.kt
+++ app/src/main/java/com/example/user/codelabkotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +20,20 @@ class MainActivity : AppCompatActivity() {
         // val myToast = Toast.makeText(this, message, duration);
         val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
         myToast.show()
+    }
+
+    fun countMe (view: View) {
+        // Get the text view
+        val showCountTextView = findViewById(R.id.textView) as TextView
+
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view.
+        showCountTextView.text = count.toString();
     }
 }
