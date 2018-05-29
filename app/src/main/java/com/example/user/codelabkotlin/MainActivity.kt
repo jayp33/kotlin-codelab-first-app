8393abfc62ea578745075e9447060aacd49c19a4
JP Hashagen <jayp33e@gmail.com>
2018-05-29 20:11:21 +0200

    Buttons and TextView

--- app/src/main/java/com/example/user/codelabkotlin/MainActivity.kt
+++ app/src/main/java/com/example/user/codelabkotlin/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.example.user.codelabkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "App launched successfully")
     }
 
-    fun changeText(view : android.view.View) {
-        val tv = findViewById<TextView>(R.id.textView)
-
-        if (tv.text.equals("Hello World!"))
-            tv.text = "Hello SoundCloud!"
-        else
-            tv.text = "Hello World!"
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
     }
 }
