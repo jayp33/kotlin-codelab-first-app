98d977276a1843b10e1ca5ed0da5c763195bee79
JP Hashagen <jayp33e@gmail.com>
2018-06-05 19:46:16 +0200

    Retrieve random number from Bundle and display in the activity

--- app/src/main/java/com/example/user/codelabkotlin/RandomActivity.kt
+++ app/src/main/java/com/example/user/codelabkotlin/RandomActivity.kt
@@ -2,12 +2,17 @@ package com.example.user.codelabkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_random.*
 
 class RandomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
+
+        val number = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        numberText.setText(number.toString())
     }
 
     companion object {
